46550fd3ada62a059f0f314a9143b57164cc01a9
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-06-10 19:10:21 +0200

    :tada: feat(client): progress on products page

--- api/src/routes/products.ts
+++ api/src/routes/products.ts
@@ -35,103 +35,121 @@ router.get("/:id", withAuth, async (req, res) => {
  * create a new product
  */
 router.post("/", withAuth, async (req: IRequest, res) => {
-  const body = req.body;
+  try {
+    const body = req.body;
 
-  const schema = createYupSchema(createProductSchema);
-  const error = await schema
-    .validate(body)
-    .then(() => null)
-    .catch((e) => e);
+    const schema = createYupSchema(createProductSchema);
+    const error = await schema
+      .validate(body)
+      .then(() => null)
+      .catch((e) => e);
 
-  if (error) {
-    return res.status(400).json({
-      error: error.message,
-      status: "error",
-    });
-  }
-
-  const existing = await prisma.product.findUnique({ where: { name: body.name } });
-
-  if (existing) {
-    return res.status(400).json({
-      error: "Name is already in-use, please update the existing item",
-      status: "error",
-    });
-  }
-
-  let category;
-  if (body.categoryId) {
-    category = await prisma.category.findUnique({
-      where: { id: body.categoryId },
-    });
-
-    if (!category) {
-      return res.status(404).json({
-        error: "That category was not found",
+    if (error) {
+      return res.status(400).json({
+        error: error.message,
         status: "error",
       });
     }
-  }
 
-  await prisma.user.update({
-    where: {
-      id: req.userId!,
-    },
-    data: {
-      products: {
-        create: {
-          name: body.name,
-          quantity: body.quantity,
-          price: body.price,
-          expirationDate: body.expirationDate || "N/A",
-          categoryId: category?.id ?? null,
+    const existing = await prisma.product.findUnique({ where: { name: body.name } });
+
+    if (existing) {
+      return res.status(400).json({
+        error: "Name is already in-use, please update the existing item",
+        status: "error",
+      });
+    }
+
+    let category;
+    if (body.categoryId) {
+      category = await prisma.category.findUnique({
+        where: { id: body.categoryId },
+      });
+
+      if (!category) {
+        return res.status(404).json({
+          error: "That category was not found",
+          status: "error",
+        });
+      }
+    }
+
+    await prisma.user.update({
+      where: {
+        id: req.userId!,
+      },
+      data: {
+        products: {
+          create: {
+            name: body.name,
+            quantity: body.quantity,
+            price: body.price,
+            expirationDate: body.expirationDate || "N/A",
+            categoryId: category?.id ?? null,
+          },
         },
       },
-    },
-  });
+    });
 
-  const products = await prisma.product.findMany();
+    const products = await prisma.product.findMany();
 
-  return res.json({ products });
+    return res.json({ products });
+  } catch (e) {
+    console.error(e);
+
+    return res.status(500).json({
+      error: "An unexpected error has occurred. Please try again later",
+      status: "error",
+    });
+  }
 });
 
 router.put("/:id", withAuth, async (req, res) => {
-  const id = req.params.id as string;
-  const body = req.body;
+  try {
+    const id = req.params.id as string;
+    const body = req.body;
 
-  const schema = createYupSchema(createProductSchema);
-  const error = await schema
-    .validate(body)
-    .then(() => null)
-    .catch((e) => e);
+    const schema = createYupSchema(createProductSchema);
+    const error = await schema
+      .validate(body)
+      .then(() => null)
+      .catch((e) => e);
 
-  if (error) {
-    return res.status(400).json({
-      error: error.message,
+    if (error) {
+      return res.status(400).json({
+        error: error.message,
+        status: "error",
+      });
+    }
+
+    const product = await prisma.product.findUnique({ where: { id } });
+
+    if (!product) {
+      return res.status(404).json({
+        error: "Product was not found",
+        status: "error",
+      });
+    }
+
+    const updated = await prisma.product.update({
+      where: { id },
+      data: {
+        name: body.name,
+        quantity: body.quantity,
+        price: body.price,
+        expirationDate: body.expirationDate || "N/A",
+      },
+    });
+
+    return res.json({ product: updated });
+  } catch (e) {
+    console.error(e);
+
+    return res.status(500).json({
+      error: "An unexpected error has occurred. Please try again later",
       status: "error",
     });
   }
-
-  const product = await prisma.product.findUnique({ where: { id } });
-
-  if (!product) {
-    return res.status(404).json({
-      error: "Product was not found",
-      status: "error",
-    });
-  }
-
-  const updated = await prisma.product.update({
-    where: { id },
-    data: {
-      name: body.name,
-      quantity: body.quantity,
-      price: body.price,
-      expirationDate: body.expirationDate || "N/A",
-    },
-  });
-
-  return res.json({ product: updated });
 });
 
 router.delete("/:id", withAuth, async (req, res) => {
