0a0fefb54a5b3e0470e97ed6c02b34de4d835ba3
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-09-10 19:59:55 +0200

    :tada: feat(views): add change view button

--- client/src/components/Products/Products.tsx
+++ client/src/components/Products/Products.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as view from "@lib/view";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
@@ -36,6 +37,7 @@ export const Products = ({ products }: Props) => {
 
   const [isOpen, setOpen] = React.useState(false);
   const [isSelectOpen, setSelectOpen] = React.useState(false);
+  const [viewType, setViewType] = React.useState(view.Views.Table);
 
   const [searchValue, setSearchValue] = React.useState<string>("");
   const [tempProduct, setTempProduct] = React.useState<Product | null>(null);
@@ -70,6 +72,10 @@ export const Products = ({ products }: Props) => {
     return sortProducts(filter.value, items);
   }, [products, searchValue, filter]);
 
+  React.useEffect(() => {
+    setViewType(view.getViewType());
+  }, []);
+
   return (
     <>
       <div className={styles.productsHeader}>
@@ -95,6 +101,15 @@ export const Products = ({ products }: Props) => {
                 {
                   name: "Import",
                   onClick: () => openModal(ModalIds.ImportProducts),
+                },
+                {
+                  name: `Show as ${view.Views[view.getNewViewType(viewType)]}`,
+                  onClick: () => {
+                    setOpen(false);
+                    const newType = view.getNewViewType(viewType);
+                    setViewType(newType);
+                    view.setViewType(newType);
+                  },
                 },
               ]}
             >
@@ -125,7 +140,7 @@ export const Products = ({ products }: Props) => {
               onClick={() => setSelectOpen((v) => !v)}
               className="btn has-icon"
             >
-              {filter?.label}{" "}
+              {filter?.label}
               <ArrowIcon style={{ transform: isSelectOpen ? "rotate(-180deg)" : "" }} />
             </button>
           </Dropdown>
@@ -157,17 +172,15 @@ export const Products = ({ products }: Props) => {
         </p>
       ) : filtered.length <= 0 ? (
         <p style={{ marginTop: "1rem" }}>No items were found with that search query.</p>
+      ) : viewType === view.Views.Table ? (
+        <ProductsTable
+          showActions
+          onManageClick={handleManage}
+          products={filtered}
+          currentFilter={filter?.value ?? null}
+        />
       ) : (
-        <div>
-          <ProductsTable
-            showActions
-            onManageClick={handleManage}
-            products={filtered}
-            currentFilter={filter?.value ?? null}
-          />
-
-          <ProductsList onManageClick={handleManage} showPagination={false} products={filtered} />
-        </div>
+        <ProductsList onManageClick={handleManage} showPagination={false} products={filtered} />
       )}
 
       <ImportProductsModal />
---
+++ client/src/lib/view.ts
@@ -0,0 +1,30 @@
+export enum Views {
+  Table,
+  List,
+}
+
+const VIEWS_KEY = "home-inventory-views-type";
+
+/**
+ * get the view type from localStorage
+ */
+export function getViewType(): Views {
+  const storage = localStorage.getItem(VIEWS_KEY);
+  if (storage) return parseInt(storage) as Views;
+
+  return Views.Table;
+}
+
+/**
+ * update the user's view type in localStorage.
+ * @param t The new theme
+ */
+export function setViewType(t: Views) {
+  localStorage.setItem(VIEWS_KEY, String(t));
+
+  return t;
+}
+
+export function getNewViewType(old: Views): Views {
+  return old === Views.Table ? Views.List : Views.Table;
+}
